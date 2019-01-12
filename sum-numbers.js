7a02b387ee383d2aac442c6030854815b301eccd
Marian Zburlea <marianzburlea@gmail.com>
2019-01-12 19:33:26 +0000

    green: sumNumbers

--- sum-numbers.js
+++ sum-numbers.js
@@ -1,10 +1,10 @@
-const sumNumbers = (a, b) => a - b
+const sumNumbers = (a, b) => a + b
 
 
 // create a simple test for sumNumbers function
 const sumResult = sumNumbers(2, 5)
-const expected = 7
+const sumExpected = 7
 
-if (sumResult !== expected) {
-  throw new Error(`${sumResult} is not equal to ${expected}`)
+if (sumResult !== sumExpected) {
+  throw new Error(`${sumResult} is not equal to ${sumExpected}`)
 }
