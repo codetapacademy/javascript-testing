5db54eb68ae3194aea4c9b281a464b4d84a35ffa
Marian Zburlea <marianzburlea@gmail.com>
2019-01-12 15:57:07 +0000

    create a simple test for sumNumbers function

--- sum-numbers.js
+++ sum-numbers.js
@@ -1,2 +1,10 @@
 const sumNumbers = (a, b) => a - b
 
+
+// create a simple test for sumNumbers function
+const sumResult = sumNumbers(2, 5)
+const expected = 7
+
+if (sumResult !== expected) {
+  throw new Error(`${sumResult} is not equal to ${expected}`)
+}
