8c172fe720ecb3b1730046a6935644a8d4dfa3f8
Marian Zburlea <marianzburlea@gmail.com>
2019-01-12 19:46:46 +0000

    read: sumResultList

--- sum-numbers.js
+++ sum-numbers.js
@@ -4,12 +4,23 @@ const substractNumbers = (a, b) => a - b
 
 
 // create a simple test for sumNumbers function
-const sumResult = sumNumbers(2, 5)
-const sumExpected = 7
+const sumResultList = [
+  sumNumbers(2, 5),
+  sumNumbers(8, 5)
+]
+const sumExpected = [
+  7,
+  14
+]
 
-if (sumResult !== sumExpected) {
-  throw new Error(`${sumResult} is not equal to ${sumExpected}`)
-}
+sumResultList.map((sumResult, key) => {
+  if (sumResult !== sumExpected[key]) {
+    throw new Error(`${sumResult} is not equal to ${sumExpected[key]}`)
+  }
+})
+
+// if (sumResult !== sumExpected) {
+// }
 
 const substrctResult = substractNumbers(8, 3)
 const substractExpected = 5
