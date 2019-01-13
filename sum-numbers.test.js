40f73db02c8ae6460899c2d2a5bcaec6218b21ed
Marian Zburlea <marianzburlea@gmail.com>
2019-01-13 19:14:50 +0000

    add prettier config in .prettierrc file

--- sum-numbers.test.js
+++ sum-numbers.test.js
@@ -1,5 +1,5 @@
-/* global test */
-import { sumNumbers, substractNumbers } from "./sum-numbers";
+/* global test, expect */
+import { sumNumbers, substractNumbers } from "./sum-numbers"
 // import { test, expect } from './global-test'
 // Clear console
 // process.stdout.write("\033c");
@@ -15,18 +15,18 @@ test("test sumNumbers function", async () => {
     await sumNumbers(2, 5, 9, 8, 1, 3),
     await sumNumbers(2, 5, 9),
     await sumNumbers(8, 5)
-  ];
-  const sumExpected = [0, 3, 7, 48, 28, 16, 13];
+  ]
+  const sumExpected = [0, 3, 7, 48, 28, 16, 13]
 
   sumResultList.map((sumResult, key) => {
-    expect(sumResult).toBe(sumExpected[key]);
-  });
-});
+    expect(sumResult).toBe(sumExpected[key])
+  })
+})
 
 test("test testSubstractNumbers function", async () => {
   // create a simple test for substractNumbers function
-  const substrctResult = await substractNumbers(8, 3);
-  const substractExpected = 5;
+  const substrctResult = await substractNumbers(8, 3)
+  const substractExpected = 5
 
-  expect(substrctResult).toBe(substractExpected);
-});
+  expect(substrctResult).toBe(substractExpected)
+})
