9b2770fbd8b89017ec4bbec7f9db46f49b74b7a6
Marian Zburlea <marianzburlea@gmail.com>
2019-01-15 23:36:04 +0000

    red: formatNumber

--- src/format-number/format-number.test.js
+++ src/format-number/format-number.test.js
@@ -6,4 +6,9 @@ describe("Format numbers", () => {
     const expected = "1,234.00"
     expect(result).toBe(expected)
   })
+  it("it should format a string to a number with two decimals", () => {
+    const result = formatNumber("67834.2")
+    const expected = "67,834.20"
+    expect(result).toBe(expected)
+  })
 })
