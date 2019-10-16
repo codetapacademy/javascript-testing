f55ee5129d0242827850b772513a78368a6bceab
Marian Zburlea <marianzburlea@gmail.com>
2019-10-16 15:46:38 +0100

    update the mock function to keep track of the arguments beeing called with

--- src/util/get-gender.test.js
+++ src/util/get-gender.test.js
@@ -1,10 +1,25 @@
 import util from '.'
 
+// this is a custom mock fat arrow function
+const f = implementation => {
+  const mockFunction = (...argumentList) => {
+    mockFunction.mock.calls.push(argumentList)
+    return implementation(...argumentList)
+  }
+
+  mockFunction.mock = {
+    calls: [],
+  }
+
+  return mockFunction
+}
+
 describe('getGender()', () => {
   it('should return male', () => {
     // mock getGender function
     const originalGetGender = util.getGender
-    util.getGender = jest.fn((male, female) => 'male')
+    util.getGender = f((male, female) => 'male')
+    // util.getGender = jest.fn((male, female) => 'male')
 
     const expected = 'male'
     const received = util.getGender('male', 'female')
@@ -12,9 +27,10 @@ describe('getGender()', () => {
     console.log(util.getGender.mock)
 
     expect(received).toBe(expected)
-    expect(util.getGender).toHaveBeenCalledTimes(1)
-    expect(util.getGender).toHaveBeenCalledWith('male', 'female')
-    expect(util.getGender).toHaveBeenNthCalledWith(1, 'male', 'female')
+    expect(util.getGender.mock.calls).toStrictEqual([ [ 'male', 'female' ] ])
+    // expect(util.getGender).toHaveBeenCalledTimes(1)
+    // expect(util.getGender).toHaveBeenCalledWith('male', 'female')
+    // expect(util.getGender).toHaveBeenNthCalledWith(1, 'male', 'female')
 
     // reset getGender function to the original
     util.getGender = originalGetGender
