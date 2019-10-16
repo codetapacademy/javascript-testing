8c7ae61f7e822646d2b71334e7ff6bcf00f07b3c
Marian Zburlea <marianzburlea@gmail.com>
2019-10-16 15:29:12 +0100

    mock using jest

--- src/util/get-gender.test.js
+++ src/util/get-gender.test.js
@@ -4,12 +4,17 @@ describe('getGender()', () => {
   it('should return male', () => {
     // mock getGender function
     const originalGetGender = util.getGender
-    util.getGender = (male, female) => 'male'
+    util.getGender = jest.fn((male, female) => 'male')
 
     const expected = 'male'
     const received = util.getGender('male', 'female')
 
+    console.log(util.getGender.mock)
+
     expect(received).toBe(expected)
+    expect(util.getGender).toHaveBeenCalledTimes(1)
+    expect(util.getGender).toHaveBeenCalledWith('male', 'female')
+    expect(util.getGender).toHaveBeenNthCalledWith(1, 'male', 'female')
 
     // reset getGender function to the original
     util.getGender = originalGetGender
