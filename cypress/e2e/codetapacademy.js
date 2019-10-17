f31142a6261f3c2a6979c7d126a95c2c9ddafb7e
Marian Zburlea <marianzburlea@gmail.com>
2019-10-17 15:20:06 +0100

    hi ha hu

--- cypress/e2e/codetapacademy.js
+++ cypress/e2e/codetapacademy.js
@@ -3,6 +3,7 @@ describe('awesome e learning', () => {
   it('shows tutorials and courses', () => {
     user
       .visit('/')
+      .queryBy("#react-will-render-here-and-it-is-really-awesome > div > div.sc-cMljjf.fuBDal > div > div > h1")
       .get(':nth-child(1) > .sc-fjdhpX > .sc-ckVGcZ')
       .click()
       .wait(1500)
